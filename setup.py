4485ab1b6831fd2ec57d09461b1b018d94594dd8
Pintu Gupta <vickygup9900@gmail.com>
2026-05-30 23:13:41 +0530

    initial

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Pintu",
+    author_email="pintukumar998075@gmail.com",
+    packages=find_packages()
+)
